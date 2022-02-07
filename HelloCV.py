c0f0da8f36993a5b771526f01d0cbfaa78b9bc15
victoryhwan0421 <victoryhwan@naver.com>
2022-02-07 20:03:35 +0900

    주석 수정

--- HelloCV.py
+++ HelloCV.py
@@ -11,12 +11,14 @@ if img is None:
     print('Image load failed!')
     sys.exit()
 
-#namedWindow(): OpenCV에서 지원하는 창(window)을 만드는 함수
-#imshow(): imageshow 의 약자, 특정 wibndow에 영상을 보여주는 함수
-# - 첫 번째 인자: 어떤 wondow에 보여줄 것인지, 
-# - 두 번째 인자: 어떤 영상을 보여줄 것인지
-#waitKey: 키보드 입력을 기다리는 함수 + 영상이 화면에 보여줄 수 있게끔 함
-#destroyAllWindows(): 기존의 모든 창 닫기
+"""
+- namedWindow(): OpenCV에서 지원하는 창(window)을 만드는 함수
+- imshow(): imageshow 의 약자, 특정 wibndow에 영상을 보여주는 함수
+    - 첫 번째 인자: 어떤 wondow에 보여줄 것인지, 
+    - 두 번째 인자: 어떤 영상을 보여줄 것인지
+- waitKey(): 키보드 입력을 기다리는 함수 + 영상이 화면에 보여줄 수 있게끔 함
+- destroyAllWindows(): 기존의 모든 창 닫기
+"""
 cv2.namedWindow('image') #image라는 창을 하나 만든다.
 cv2.imshow('image', img) #image 창에 img 영상을 보여주기
 cv2.waitKey()
